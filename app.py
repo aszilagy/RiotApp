fa646bda0119657e8a2fe50e9cbfd469d86524f0
DESKTOP-5J2VUUC\Alex <aszilagy@hawk.iit.edu>
2019-03-04 17:30:18 -0600

    port host moved to config

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ config.read('config.ini')
 
 encrypted_pass = config['DEFAULT']['PASSWORD']
 encrypted_user = config['DEFAULT']['USER']
+port = config['DEFAULT']['PORT']
+host = config['DEFAULT']['HOST']
 tournieId = ""
 
 blueTeam = []
@@ -98,4 +100,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host=host, port=port)
