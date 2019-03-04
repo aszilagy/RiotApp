ed317a5b27247e04aea6dd96e3c165ee815d532b
DESKTOP-5J2VUUC\Alex <aszilagy@hawk.iit.edu>
2019-03-04 16:56:23 -0600

    v0.1

---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, render_template, request
+import riot_app as ri
+from passlib.hash import sha256_crypt as sha
+import configparser as cf
+
+config = cf.ConfigParser()
+config.read('config.ini')
+
+encrypted_pass = config['DEFAULT']['PASSWORD']
+encrypted_user = config['DEFAULT']['USER']
+tournieId = ""
+
+blueTeam = []
+redTeam = []
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def login():
+
+    return render_template('login.html')
+
+@app.route('/tourney/', methods=['GET','POST'])
+def verify_login():
+    if 'username' in request.form and 'password' in request.form:
+        checkUser = sha.verify(request.form['username'], encrypted_user)
+        checkPass = sha.verify(request.form['password'], encrypted_pass)
+    else:
+        username = 'guest'
+
+    if checkUser == True and checkPass == True:
+        return render_template('index.html', username=request.form['username'])
+    
+    elif checkUser == False or checkPass == False:
+        return render_template('login.html',error='Wrong Username/Password')
+
+    else:
+        return render_template('login.html')
+
+
+@app.route('/refresh_tourney/', methods=['GET','POST'])
+def refresh_tourney():
+    global tournieId, blueTeam, redTeam;
+    if tournieId == "":
+        tournieId = ri.get_provider_id()
+
+    jData = ri.get_events(tournieId)
+
+    sortedEvents = sorted(jData['eventList'], key = lambda k: k['timestamp'])
+
+    for obj in sortedEvents:
+        summoner, eventData = obj['summoner'], obj['eventType']
+
+        if eventData == 'PlayerJoinedGameEvent' and obj['summonerId'] != None:
+            if summoner not in blueTeam:
+                summoner.team = 1
+                blueTeam.append(summoner)
+
+            if summoner in redTeam:
+                redTeam.remove(summoner)
+
+        if eventData == 'PlayerSwitchedTeamEvent' and obj['summonerId'] != None:
+
+            if summoner not in redTeam:
+                summoner.team = 2
+                redTeam.append(summoner)
+
+            if summoner in blueTeam:
+                blueTeam.remove(summoner)
+            
+        if eventData == 'PlayerQuitGameEvent' and obj['summonerId'] != None:
+            if summoner in blueTeam:
+                print("Found summoner on blue team that left game")
+                blueTeam.remove(summoner)
+
+            elif summoner in redTeam:
+                print("Found summoner on red team that left game")
+                redTeam.remove(summoner)
+
+            else:
+                print("Summoner not in game")
+
+    return render_template('index.html', tourn_id = tournieId, eventList = jData['eventList'], blueTeam = blueTeam, redTeam = redTeam)
+
+
+@app.route('/get_tourney/', methods=['GET','POST'])
+def get_tourney():
+    global tournieId
+    print("Working")
+    tournieId = ri.get_provider_id()
+    print(tournieId)
+
+    return render_template('index.html',tourn_id=tournieId)
+
+
+@app.route('/remind/', methods=['GET','POST'])
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ riot_app.py
@@ -0,0 +1,148 @@
+import requests
+import json
+import pprint
+import flask
+import configparser as cf
+
+config = cf.ConfigParser()
+config.read('config.ini')
+
+api_key = config['DEFAULT']['API_KEY']
+
+def main():
+    #get_summoner_info('Belgian Wofls')
+    tourn = get_provider_id()
+
+    get_events(tourn)
+
+def get_events(tourn):
+    url = 'https://americas.api.riotgames.com/lol/tournament-stub/v4/lobby-events/by-code/' + tourn 
+    params = {"api_key": api_key}
+    myReq = requests.get(url, params=params, verify=True)
+    jData = myReq.json()
+
+    for j in jData['eventList']:
+        print(j['timestamp'], j['summonerId'], j['eventType'])
+        if j['summonerId'] != None:
+            #print(j['summonerId'])
+            #XXX This should be put back in once you have actual summoner Ids
+            #XXX summoner = get_summoner_by_id(j['summonerId'])
+            j['summoner'] = get_summoner_info('Decelx')
+
+        else:
+            j['summoner'] = None
+
+    print(jData)
+
+    return jData
+
+def get_provider_id():
+    url = 'https://americas.api.riotgames.com/lol/tournament-stub/v4/providers?api_key='+api_key
+    data = {"url": "http://18.232.99.218/", "region":"NA"}
+    myReq = requests.post(url, data=json.dumps(data), verify=True)
+    jprint(myReq)
+
+    jsonReq = myReq.json() #158 <- PROVIDER ID
+
+    tourn = get_tourn_id(jsonReq) #158
+
+    return tourn
+
+def get_tourn_id(tournId:int):
+    url = 'https://americas.api.riotgames.com/lol/tournament-stub/v4/tournaments'
+    params = {"api_key": api_key}
+    data = {"name":"Friends-Game-Night", "providerId":tournId}
+    myReq = requests.post(url, data=json.dumps(data), params=params, verify=True)
+    jprint(myReq)
+
+    jsonReq = myReq.json() #3968 <- TOURNAMENT ID
+
+    tourn = create_tourney_game(jsonReq)
+
+    return tourn
+
+def create_tourney_game(tournId):
+    url = 'https://americas.api.riotgames.com/lol/tournament-stub/v4/codes'
+    params = {"api_key": api_key, "tournamentId": tournId}
+    data = {"teamSize":5, "mapType":"SUMMONERS_RIFT", "pickType": "DRAFT_MODE", "spectatorType":"ALL"}
+
+    headers = {'content-type':'application/json'}
+    print('Data object', data)
+    myReq = requests.post(url, data=json.dumps(data), params=params, verify=True)
+
+    print("Creating mock tourney")
+    #jprint(myReq)
+    tourn_id = myReq.json()[0]
+
+    return tourn_id
+
+
+def jprint(jsonD):
+    '''
+    Parameters: Request ... myReq  [Returned from requests.get]
+    '''
+    jData = json.loads(jsonD.content)
+    print(json.dumps(jData, indent=4))
+
+def parse_summoners(ln):
+    ln = ['Decelx', 'Finland Wofls']
+
+def get_summoner_mastery(summoner_id: str):
+    pass
+
+def get_summoner_by_id(summoner_id):
+    url = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/'+ summoner_id + '?api_key='+api_key
+    myReq = requests.get(url, verify=True)
+    if(myReq.ok):
+        jDat = json.loads(myReq.content)
+        jprint(myReq)
+        json_to_summoner(myReq)
+
+    else:
+        print("SUMMONER_INFO BAD")
+        jprint(myReq)
+
+    return myReq.json()
+
+def get_summoner_info(summoner_name):
+    url = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ summoner_name + '?api_key='+api_key
+    myReq = requests.get(url, verify=True)
+    if(myReq.ok):
+        jDat = json.loads(myReq.content)
+        summonerObj = json_to_summoner(myReq)
+        return summonerObj
+
+    else:
+        print("SUMMONER_INFO BAD")
+
+def json_to_summoner(jsonD):
+    jData = json.loads(jsonD.content)
+    sumObj = Summoner(jData['profileIconId'], 
+            jData['name'],
+            jData['puuid'],
+            jData['summonerLevel'],
+            jData['revisionDate'],
+            jData['id'],
+            jData['accountId'])
+
+    return sumObj
+
+class Summoner:
+    def __init__(self,
+            profileIconId: int,
+            name: str,
+            puuid: str,
+            summonerLevel: int,
+            revisionDate: int,
+            summonerId: str,
+            accountId: str):
+        self.icon = profileIconId
+        self.name = name
+        self.puuid = puuid
+        self.level = summonerLevel
+        self.revDate = revisionDate
+        self.summonerId = summonerId
+        self.accountId = accountId
+
+if __name__ == '__main__':
+    main()
